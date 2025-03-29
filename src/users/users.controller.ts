695454ebef9b7841ec98da086da47af0d1e58e42
AxFerCg13 <axfervagar@gmail.com>
2025-03-28 22:49:21 -0600

    feat: create imageFilter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto, LoginDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/mongo-id/mongo-id.pipe';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { imageFilter } from 'src/common/helpers/fileFilter';
 
 @Controller('users')
 export class UsersController {
@@ -24,6 +26,12 @@ export class UsersController {
   }
 
   @Patch(':userId/image')
+  @UseInterceptors(FileInterceptor('companyImage', {
+    limits: {
+      fileSize: 1024 * 1024 * 1 // 1 MB 
+    },
+    fileFilter: imageFilter,
+  }))
   uploadImage(
     @Param('id', ParseMongoIdPipe) id: string
   ){

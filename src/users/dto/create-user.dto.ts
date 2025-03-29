ceeec538873f1e8f21daff1fa802b6ad131b5ab2
AxFerCg13 <axfervagar@gmail.com>
2025-03-28 20:12:08 -0600

    feat: create dto to create one user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,33 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, IsString, Matches } from "class-validator";
+
+enum UserType {
+    PUBLICUSER = "publicuser", 
+    ORGANIZATON = "organization",  
+}
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    address: string;
+
+    @IsString()
+    @IsNotEmpty()
+    phoneNumber: string;
+
+    @IsString()
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
+    @IsNotEmpty()
+    password: string;
+
+    @IsEnum(UserType)
+    @IsNotEmpty()
+    type: UserType
+}

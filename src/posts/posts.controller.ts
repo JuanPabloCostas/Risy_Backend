168ede970dfe8c531e3c59beec28f6c9679079f0
JuanPabloCostas <costasrueda.juan@gmail.com>
2025-03-29 00:37:01 -0600

    Feat: bull

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { ObjectId } from 'mongodb';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  async create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
   }
 
   @Get()
-  findAll() {
-    return this.postsService.findAll();
+  async listAllPosts() {
+    return this.postsService.listAllPosts();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+  @Get('/:id')
+  async findOne(@Param('id') id: string) {
+    return this.postsService.findOne(new ObjectId(id));
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.update(+id, updatePostDto);
+  @Put('/:id')
+  async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(new ObjectId(id), updatePostDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postsService.remove(+id);
+  @Delete('/:id')
+  async delete(@Param('id') id: string) {
+    return this.postsService.delete(new ObjectId(id));
   }
 }

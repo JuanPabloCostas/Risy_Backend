b37785dea1fa18256487a913509507046463c6d7
AxFerCg13 <axfervagar@gmail.com>
2025-03-28 18:55:48 -0600

    feat: set attributes to providers entity

--- src/providers/entities/provider.entity.ts
+++ src/providers/entities/provider.entity.ts
@@ -1,1 +1,27 @@
-export class Provider {}
+import { Column, ObjectIdColumn } from "typeorm";
+
+export class Provider {
+    @ObjectIdColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    phoneNumber: string;
+
+    @Column()
+    photoUrl: string;
+
+    @Column()
+    password: string;
+    
+    @Column()
+    registeredAt: Date;
+}

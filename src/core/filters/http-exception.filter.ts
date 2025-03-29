4da9c4c69699b9bbb44d84836447e486ad422249
JuanPabloCostas <costasrueda.juan@gmail.com>
2025-03-28 20:23:03 -0600

    Feat: fix

--- src/core/filters/http-exception.filter.ts
+++ src/core/filters/http-exception.filter.ts
@@ -8,8 +8,6 @@ export class HttpExceptionFilter implements ExceptionFilter {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
-
-        console.log(request);
         
 
         let httpException: HttpException;
